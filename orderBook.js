b7f0bd64d5a07360dfaceb15922c17ae1c4477e2
carrye <carryegaulin033178@gmail.com>
2020-11-05 22:40:27 -0500

    code cleaned and ready for push

--- orderBook.js
+++ orderBook.js
@@ -2,10 +2,10 @@ function reconcileOrder(book, incomingOrder) {
   for (let i = 0; i < book.length && incomingOrder.quantity > 0; i++) {
     if (book[i].type !== incomingOrder.type &&
       book[i].price === incomingOrder.price) {
-      let [order] = book.splice(i, 1) // removes the order and destructures it from the array to be a single object
+      let [order] = book.splice(i, 1)
 
       fulfillOrders(order, incomingOrder, book)
-      i-- // rolls back the i variable as we have removed an item from the array
+      i--
     }
   }
   book.push(incomingOrder)
@@ -13,23 +13,19 @@ function reconcileOrder(book, incomingOrder) {
   return book.filter(order => order.quantity !== 0)
 }
 
-// for this function, we know the order objects have the same type && price
 function fulfillOrders(existingOrder, incomingOrder, book,) {
-  // existingBook = object, incomingOrder = objec
   if (existingOrder.quantity === incomingOrder.quantity) {
-    existingOrder.quantity = 0 // setting the existingOrder quantity VALUE to 0 signifies that it is fulfilled. and start at beginning of array again??
-    incomingOrder.quantity = 0 // setting the incomingOrder's quantity to 0 signifies that it is fulfilled. and start at beginning of the array again??
+    existingOrder.quantity = 0
+    incomingOrder.quantity = 0
   } else if (existingOrder.quantity < incomingOrder.quantity) {
     incomingOrder.quantity -= existingOrder.quantity
     existingOrder.quantity = 0
   } else {
     existingOrder.quantity -= incomingOrder.quantity
-    incomingOrder.quantity = 0 // existingOrder.quantity > incomingOrder.quantity
+    incomingOrder.quantity = 0
 
     book.push(existingOrder)
   }
-
-  // return book
 }
 
 module.exports = reconcileOrder
